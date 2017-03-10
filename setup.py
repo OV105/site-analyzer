1f5006f05dbb829241dd2db0ca82e4289f727b76
Tim Lee <tlee@new-host-2.home>
2017-03-10 10:07:53 -0500

    setup for pypi

--- setup.py
+++ setup.py
@@ -2,23 +2,28 @@ import os
 from setuptools import setup
 
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    try:
+        return open(file_path).read()
+    except IOError as ex:
+        print("Error '{}' for file {}".format(ex, file_path))
+        raise ex
 
 setup (
     name = "analyze_site",
-    version = "0.1",
+    version = "0.1.3",
     author = "Tim Lee",
     author_email = "timhl81@gmail.com",
     description = "Utility to crawl web site looking for key words",
     license = "Apache 2.0",
-    keywords = "web crawl analyze",
+    keywords = ["web","crawl","analyze"],
     url = "https://github.com/OV105/site-analyzer",
     long_description = read("README.md"),
     classifiers = [
         "Topic :: Utilities",
-        "License :: OSI Approved :: Apache 2.0",
-        "Development Status :: 4 Beta"
+        "License :: OSI Approved :: Apache Software License",
+        "Development Status :: 4 - Beta"
     ],
-    py_modules = ["analyze_site"]
-
+    py_modules = ["analyze_site"],
+    install_requires = [ "nltk"]
 )
